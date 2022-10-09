2c26f4e4b351085663918f5519e3d0d34722793f
JCaloca <jacobcaloca1@gmail.com>
2022-10-09 16:05:21 -0700

    added div for search results to be appended via js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 var APIKey = "e4355a4446dc417d0aad89e95bf488f5";
 var city;
-var state;
-var country;
+
+
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid="+ APIKey;
+
+fetch(queryURL);
